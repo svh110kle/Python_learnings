0fa863ee80ea01a85e69bffed65fe53e3c4dc304
ShaileshVHegde <shaileshhegdesvh@gmail.com>
2026-02-03 22:35:34 +0530

    if __name__==main condition

---
+++ SVHMODULE.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+print("SVHMODULE.py loaded successfully.")
+
+if __name__ == "__main__":
+    print("Addition of 5 and 3:", add(5, 3))
+    print("Subtraction of 5 from 10:", subtract(10, 5))
---
+++ if__name==main.py
@@ -0,0 +1,9 @@
+import SVHMODULE
+
+SVHMODULE.add(10, 20)
+SVHMODULE.subtract(30, 15)
+
+print("Main script executed.")
+
+#we use if __name__ == "__main__": to check whether the script is being run directly or being imported as a module.
+#if there is no such condition, the code will execute regardless of how the script is run.

b8ee73db40272f5077080ac6eb4b68e44edda626
ShaileshVHegde <shaileshhegdesvh@gmail.com>
2026-02-05 22:53:24 +0530

    gobal and local var

---
+++ Global and Local Variable.py
@@ -0,0 +1,15 @@
+x=10 #globlal variable
+
+def my_function1():
+    y=5
+    print("The value of x is:",x) #accessing global variable
+    
+my_function1()
+# to cahnge the value of global variable inside a function we need to use global keyword
+def my_function():
+    global x #declaring x as global variable
+    x=20 #changing the value of global variable
+    print("The value of x inside the function is:",x)
+    
+my_function()
+print("The value of x outside the function is:",x) #accessing the changed value of global variable

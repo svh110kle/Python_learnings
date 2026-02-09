4d98c65bff15efd79d1feefc737e3f2459612645
ShaileshVHegde <shaileshhegdesvh@gmail.com>
2026-02-10 00:00:39 +0530

    lamda

---
+++ lambda.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    return x+y
+#we can also write the above function as a lambda function
+add= lambda x,y: x+y
+print(add(5,10))
+
+double= lambda x: x*2
+print(double(5))
+triple= lambda x: x*3
+print(triple(5))

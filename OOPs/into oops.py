3cd52a725c055d60a3334aaab8b1b596a280d3af
ShaileshVHegde <shaileshhegdesvh@gmail.com>
2026-02-13 23:36:42 +0530

    into opps

---
+++ OOPs/into oops.py
@@ -0,0 +1,6 @@
+#object orinted programming is a programming paradigm that uses "objects" to design applications and computer programs. It utilizes several techniques from previously established paradigms, including modularity, polymorphism, and encapsulation. The main concepts of OOP include:
+#1. Class: A blueprint for creating objects. It defines a set of attributes and methods
+#2. Object: An instance of a class. It is a self-contained entity that consists of both data and procedures to manipulate the data.
+#3. Inheritance: A mechanism that allows a new class to inherit properties and behavior from an existing class.
+#4. Polymorphism: The ability of different classes to be treated as instances of the same class through a common interface. It allows for methods to be used on objects of different types.
+#5. Encapsulation: The bundling of data and methods that operate on the data within
